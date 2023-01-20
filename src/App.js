614780cadad23eb144f10ab96bc05a1e5787ca1a
Lucie Sundermeijer <lucie.sun@gmail.com>
2023-01-20 17:21:45 +0100

    app setting
    content + style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-import './index.css'
+import { useState } from "react"
+import axios from "axios"
 
 function App() {
+
+  //const url=`https://api.openweathermap.org/data/2.5/weather?q=niort&appid=5ec51e08222cd23e5141dbe0ca06927d`
+  
+  
   return (
-    <div className="App">
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <h1>Ville</h1>
+          </div>
+          <div className="temp">
+            <h2>Température</h2>
+          </div>
+          <div className="description">
+            <p>Description temps</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p>Ressenti température</p>
+          </div>
+          <div className="humidity">
+            <p>Humidité</p>
+          </div>
+          <div className="wind">
+            <p>Vents</p>
+          </div>
+        </div>
+      </div>
       
     </div>
   )
